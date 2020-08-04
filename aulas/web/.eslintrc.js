7c91d7ca50162c85f3992bc00015c456e4940053
Renan Oliveira <renan04lima@outlook.com>
2020-08-04 00:20:28 -0300

    configurações/dependências, assets e global.css

---
+++ aulas/web/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    "import/no-unresolved": [2, {"commonjs": false, "amd": false}],
+    "react/jsx-filename-extension": [1, { "extensions": [".ts", ".tsx"] }]
+  }
+};
