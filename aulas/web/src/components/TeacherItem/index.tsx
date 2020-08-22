ce69a10f4fce1f7d6ca5c7801b71f3a8c8a33c62
Renan Oliveira <renan04lima@outlook.com>
2020-08-22 19:32:58 -0300

    :package: feat: buscar de professores

--- aulas/web/src/components/TeacherItem/index.tsx
+++ aulas/web/src/components/TeacherItem/index.tsx
@@ -4,38 +4,47 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
-  return (
-    <article className="teacher-item">
-      <header>
-        <img src="https://avatars1.githubusercontent.com/u/48491038?s=460&u=7ce2439da5347b9068d2cf382c04fb287d6b2460&v=4" alt="avatar" />
-        <div>
-          <strong>Renan Oliveira</strong>
-          <span>Computação</span>
-        </div>
-      </header>
-
-      <p>
-        Entusiasta das melhores tecnologias da computação.
-        <br />
-        {' '}
-        <br />
-        My name is Renan Oliveira, and I'm from Brazil (brazil). I'm student of Computer Science at University Federal of São Carlos since 2019 and currently I'm Web Developer at Associação Share.
-      </p>
-
-      <footer>
-        <p>
-          Preço/hora
-          <strong>R$ 90,00</strong>
-        </p>
-        <button type="button">
-          <img src={whatsappIcon} alt="Whatsapp" />
-          Entrar em contato
-        </button>
-      </footer>
-
-    </article>
-  );
+export interface Teacher {
+  avatar: string,
+  bio: string,
+  cost: number,
+  id: number,
+  name: string,
+  subject: string,
+  whatsapp: string,
 }
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => (
+  <article className="teacher-item">
+    <header>
+      <img src={teacher.avatar} alt={teacher.name} />
+      <div>
+        <strong>{teacher.name}</strong>
+        <span>{teacher.subject}</span>
+      </div>
+    </header>
+
+    <p>
+      {teacher.bio}
+      {' '}
+    </p>
+
+    <footer>
+      <p>
+        Preço/hora
+        <strong>R$ {teacher.cost}</strong>
+      </p>
+      <button type="button">
+        <img src={whatsappIcon} alt="Whatsapp" />
+        Entrar em contato
+      </button>
+    </footer>
+
+  </article>
+);
 
 export default TeacherItem;
